bb4e82fb0dcae147aa84a9ebc646a2fcf9536137
hannahnier <hannahr@posteo.de>
2024-04-18 12:06:24 +0200

    add fetch functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [loadData, setLoadData] = useState(false);
+
+  const url = "https://api.openbrewerydb.org/v1/breweries";
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(url);
+        console.log(res);
+        const dataJson = await res.json();
+        setData(dataJson);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const clickHandler = () => {
+    setLoadData(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Get your favourite beer</h1>
+
+      <button onClick={clickHandler}>Find your local brewer!</button>
+
+      {loadData &&
+        data.map((x) => (
+          <div key={x.id}>
+            <h3>
+              {x.name} ({x.brewery_type})
+            </h3>
+            <a href={x.website_url} target="_blank">
+              Visit website
+            </a>
+            <p>{x.state}</p>
+          </div>
+        ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
